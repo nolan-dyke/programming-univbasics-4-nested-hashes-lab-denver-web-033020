8a2b5cf36889561d4f62d0636924f88f03773dce
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-02-22 20:09:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,28 +77,6 @@ def adding_matz
      }
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
   return programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-    
 end
 
 def changing_alan
